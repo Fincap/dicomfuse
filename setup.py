b2399a293945cdd1dfa98e69a20a8e4ee6ea799d
fincap <finnkerman@gmail.com>
2021-04-07 23:29:18 +1000

    Create basic setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='dicomfuse',
+  version='0.0.1',
+  description='Simple library for finding affine translation matrix between two DICOM images.',
+  url='n/a',
+  author='Matthew Archer',
+  author_email='matthew.e.archer9@gmail.com',
+  packages=find_packages(where='dicomfuse'),
+  python_requires='>=3.5'
+)
