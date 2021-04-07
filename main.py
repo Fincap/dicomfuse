147f3a8f1973c8b673d7e104ca2077bb138ab271
fincap <finnkerman@gmail.com>
2021-04-08 00:58:27 +1000

    Updated main to test solve affine

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-import dicomfuse
+import numpy as np
+
+from dicomfuse.utils import get_transform_function
+
+primary_points = [
+  (1, 1),
+  (2, 2),
+  (3, 3),
+  (4, 4)
+]
+
+secondary_points = [
+  (1, 2.1),
+  (2.8, 3.2),
+  (3, 4.3),
+  (4, 5.4)
+]
+
+
+transform_function = get_transform_function(primary_points, secondary_points)
+
+result = transform_function(primary_points[1])
+print(result)
